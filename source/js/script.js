56d0b731982ef6948612aa3bfd0734d538e15fea
Maria <mi.kovalenko@mail.ru>
2023-10-20 18:51:31 +0400

    formdesktop

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,21 @@
 const toggleButton = document.getElementById('burger-menu')
 const toggleContainer = document.querySelector(".header__toggle-container");
+const menu = document.getElementsByClassName('menu-list')[0]
+
 
 function toggleMenu() {
-  toggleContainer.classList.toggle('open')
+  toggleContainer.classList.toggle('open');
+  menu.classList.toggle('shown')
 }
 
 toggleButton.addEventListener("click", (toggleMenu))
+
+document.addEventListener("click", function(e) {
+  if((!toggleContainer.contains(e.target) && !menu.contains(e.target))) {
+    if (toggleContainer.classList.contains('open')) {
+      toggleContainer.classList.remove('open');
+      menu.classList.remove('shown')
+    }
+  }
+})
+
