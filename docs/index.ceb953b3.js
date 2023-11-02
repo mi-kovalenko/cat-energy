1e12e716c20072714f9153a04e0ad19f66811c3d
Maria <mi.kovalenko@mail.ru>
2023-11-02 23:06:37 +0400

    kinda fixed it?

---
+++ docs/index.ceb953b3.js
@@ -0,0 +1,3 @@
+const t=L.map("map").setView([41.72931,44.769338],15);L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(t);const e=L.icon({iconUrl:"../img/map-pin.png",iconSize:[100,100]});L.marker([41.72931,44.769338],{icon:e}).addTo(t);//# sourceMappingURL=index.ceb953b3.js.map
+
+//# sourceMappingURL=index.ceb953b3.js.map
